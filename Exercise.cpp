6993690b2d327d256f22cb674a566ebd66ce0c2f
Default user <yalejandro.gomez@gmail.com>
2019-11-11 18:59:29 +0000

    Actualizar ejercicio final

--- Exercise.cpp
+++ Exercise.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <cmath>
+#include <iomanip> 
 
 int fill(int num,float **M);
 int imprimir(int f,int c,float **M);
 float **mul(int n1,int m1,int n2,int m2,float **M,float **N);
+bool pruebaNil(int num,float **M);
 
 int main()
 {
@@ -27,18 +28,29 @@ int main()
     
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-            H[i][j] = 1/(i+j-1);
+            H[i][j] = float(1)/float(i+j+1);
         }
     }
-    std::cout<<"Matriz de Hilbert(H):"<<std::endl;
+    
+    std::cout<<"Matriz de Hilbert (H):"<<std::endl;
     imprimir(N,N,H);
+    
     R = mul(N,N,N,N,H,H);
     std::cout<<"Multiplicación H*H:"<<std::endl;
     imprimir(N,N,R);
     
     fill(N,M);
-    std::cout<<"Matriz nilpotente:"<<std::endl;
+    std::cout<<"Matriz nilpotente (N):"<<std::endl;
     imprimir(N,N,M);
+    
+    bool prueba;
+    prueba = pruebaNil(N,M);
+    if(prueba){
+        std::cout<<"La matriz N es Nilpotente"<<std::endl;
+    }
+    else{
+        std::cout<<"La matriz N NO es Nilpotente"<<std::endl;
+    }
 
     return 0;
 }
@@ -67,8 +79,10 @@ int fill(int num,float **M){
 }
 
 int imprimir(int f,int c,float **M){
+    std::cout<<std::setprecision(16);
     for (int i=0;i<f;i++){
         for (int j=0;j<c;j++) {
+            std::cout<<std::scientific;
             std::cout<<M[i][j]<<"\t";
         }
         std::cout<<std::endl;
@@ -100,4 +114,28 @@ float **mul(int n1,int m1,int n2,int m2,float **M,float **N){
     else{
         std::cout<<"No se puede realizar la multiplicación, m1!=n2"<<std::endl;
     }
+}
+
+bool pruebaNil(int num,float **M){
+    bool prueba = false;
+    float **R = new float *[num];
+        for (int i=0;i<num;++i){
+            R[i] =new float[num];
+        }
+    R = mul(num,num,num,num,M,M);
+    for(int k=0;k<num;k++){
+        for(int i=0;i<num;i++){
+            for(int j=0;j<num;j++){
+                if(R[i][j]==0){
+                    prueba = true;
+                }
+                else{
+                    prueba = false;
+                }
+            }
+        }
+        R = mul(num,num,num,num,R,M);
+    }
+    
+    return prueba;
 }
